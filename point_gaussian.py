0ea6430df3ff723c58085c812cd6e193998e5869
ariva00 <ariva00.it@gmail.com>
2024-03-18 18:37:58 +0100

    Add point gaussian function

---
+++ point_gaussian.py
@@ -0,0 +1,56 @@
+import torch
+import trimesh
+import numpy as np
+
+def point_gauss(x:torch.Tensor, y:torch.Tensor, sigma:float):
+    dist = torch.cdist(x, y, p=1)
+    return ((-(dist**2)/(2*(sigma**2))).exp())
+
+if __name__ == "__main__":
+    import os
+    import random
+    import plotly.graph_objects as go
+
+    points = torch.from_numpy(trimesh.load_mesh((os.path.join('dataset', '12ktemplate.ply')),
+                                                            process=False).vertices).float()
+    points = torch.from_numpy(np.load(os.path.join('dataset', '12k_shapes_train.npy'))).float()
+    points = points[[
+        random.randint(0, points.shape[0]),
+        random.randint(0, points.shape[0])
+    ]]
+
+    p = torch.stack((
+        points[0][random.randint(0, points.shape[1])],
+        points[1][random.randint(0, points.shape[1])]
+    ))
+
+    p = torch.stack((
+        points[0][random.randint(0, points.shape[1])],
+        points[1][random.randint(0, points.shape[1])]
+    ))
+
+    p = p.unsqueeze(1)
+    print(points.shape)
+    print(p.shape)
+
+    y = point_gauss(points, points, 0.2)
+
+    print(y.shape)
+
+    fig = go.Figure(
+        data=[
+                go.Scatter3d(
+                    x=points[0,:,0], y=points[0,:,2], z=points[0,:,1],
+                    mode='markers',
+                    marker=dict(size=3, color=y[0,:,0], colorscale='jet', opacity=0.8)
+                ),
+                go.Scatter3d(
+                    x=points[1,:,0]+1, y=points[1,:,2], z=points[1,:,1],
+                    mode='markers',
+                    marker=dict(size=3, color=y[1,:,0], colorscale='jet', opacity=0.8)
+                )
+            ],
+        layout = go.Layout(scene=dict(aspectmode='data'))
+    )
+
+    fig.show()
